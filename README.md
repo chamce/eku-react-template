9565bec1cc38746488ed63adc54b79c963b1c372
Chamce <chancellor.whitaker@eku.edu>
2023-05-08 12:04:48 -0400

    docs for gh pages

--- README.md
+++ README.md
@@ -16,8 +16,8 @@ And then open http://localhost:5173 to view it in the browser.
 
 In this project, you can run the following scripts:
 
-| Script        | Description                                         |
-| ------------- | --------------------------------------------------- |
-| npm run dev   | Runs the app in the development mode.               |
-| npm run build | Builds the app for production to the `dist` folder. |
-| npm run serve | Serves the production build from the `dist` folder. |
+| Script          | Description                                         |
+| --------------- | --------------------------------------------------- |
+| npm run dev     | Runs the app in the development mode.               |
+| npm run build   | Builds the app for production to the `dist` folder. |
+| npm run preview | Serves the production build from the `dist` folder. |
--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
-// Uncomment lines above plugins to deploy build outside of root directory
+// Uncomment emptyOutDir if you need to set outDir outside of root directory
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // // Produces relative links to assets in your index.html
-  // base: './',
-  // build: {
-  //   // By default, Vite will empty the outDir on build if it is inside project root. It will emit a warning if outDir is outside of root to avoid accidentally removing important files. You can explicitly set this option to suppress the warning. This is also available via command line as --emptyOutDir
-  //   emptyOutDir: true,
-  //   // Specify the output directory (relative to project root)
-  //   outDir: "Y:/Your/Build/Directory"
-  // },
-  plugins: [react(), eslint()],
+  base: './',
+  build: {
+    outDir: './docs',
+    // emptyOutDir: true
+  },
+  plugins: [react(), eslint()]
 });
