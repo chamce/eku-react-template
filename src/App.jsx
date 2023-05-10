3b73182da839cd8dff1147577bf92e7a0b97abe1
Chamce <chancellor.whitaker@eku.edu>
2023-05-10 09:37:43 -0400

    prettier ignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import { ExampleDashboard } from './ExampleDashboard';
 
+// data files go in public folder
+
 // replace the default modal header button (close button) with the dynamicModalButton prop to have a button which automatically turns into a back button when travelling from the fullscreen modal to another modal
 // Bootstrap Modal Usage: https://getbootstrap.com/docs/5.3/components/modal/
 
