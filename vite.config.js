92c49d20d730a2cdcc0a926932abe55c5888f864
Chamce <chancellor.whitaker@eku.edu>
2023-05-11 16:07:31 -0400

    made vite.config easier

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
-// Uncomment emptyOutDir if you need to set outDir outside of root directory
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   build: {
     outDir: './docs',
-    // emptyOutDir: true
+    emptyOutDir: true,
   },
   plugins: [react(), eslint()],
 });
